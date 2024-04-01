fc2a59e49ead42550d654df2fa2f1e2c72276a57
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2024-04-02 00:36:10 +0800

    init: raise concern

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,66 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _showForm = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home'),
+        title: Text('Community'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Placeholder for search functionality
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Search'),
+                    content: Text('Placeholder for search functionality'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('OK'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
           children: <Widget>[
-            Text(
-              'Welcome to the Citizen App!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Menu',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
+            ListTile(
+              title: Text('Report an Issue'),
+              onTap: () {
                 // Placeholder for reporting issues feature
-                // Replace this with the actual functionality
+                Navigator.pop(context); // Close the drawer
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
@@ -38,13 +79,12 @@ class HomePage extends StatelessWidget {
                   },
                 );
               },
-              child: Text('Report an Issue'),
             ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
+            ListTile(
+              title: Text('Government Resources'),
+              onTap: () {
                 // Placeholder for accessing government resources feature
-                // Replace this with the actual functionality
+                Navigator.pop(context); // Close the drawer
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
@@ -63,13 +103,12 @@ class HomePage extends StatelessWidget {
                   },
                 );
               },
-              child: Text('Government Resources'),
             ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
+            ListTile(
+              title: Text('Civic Matters'),
+              onTap: () {
                 // Placeholder for getting updates on civic matters feature
-                // Replace this with the actual functionality
+                Navigator.pop(context); // Close the drawer
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
@@ -88,11 +127,167 @@ class HomePage extends StatelessWidget {
                   },
                 );
               },
-              child: Text('Civic Matters'),
             ),
           ],
         ),
       ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: _showForm ? _buildForm() : _buildWelcomeMessage(),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _showForm = true;
+          });
+        },
+        child: Icon(Icons.add),
+      ),
     );
+  }
+
+  Widget _buildWelcomeMessage() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          'Welcome to the Citizen App!',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'Your Complaint',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          DropdownButtonFormField<String>(
+            decoration: InputDecoration(
+              labelText: 'Type of Community Problem',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            items: [
+              DropdownMenuItem(
+                value: 'option1',
+                child: Text('Option 1'),
+              ),
+              DropdownMenuItem(
+                value: 'option2',
+                child: Text('Option 2'),
+              ),
+              DropdownMenuItem(
+                value: 'option3',
+                child: Text('Option 3'),
+              ),
+            ],
+            onChanged: (value) {},
+          ),
+          SizedBox(height: 20),
+          ElevatedButton.icon(
+            onPressed: () {
+              // Placeholder for uploading photo or video
+            },
+            icon: Icon(Icons.camera_alt),
+            label: Text('Upload Photo or Video'),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Write Your Problem',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            maxLines: 3,
+          ),
+          SizedBox(height: 20),
+          DropdownButtonFormField<String>(
+            decoration: InputDecoration(
+              labelText: 'Purok Location (Optional)',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            items: [
+              DropdownMenuItem(
+                value: 'purok1',
+                child: Text('Purok 1'),
+              ),
+              DropdownMenuItem(
+                value: 'purok2',
+                child: Text('Purok 2'),
+              ),
+              DropdownMenuItem(
+                value: 'purok3',
+                child: Text('Purok 3'),
+              ),
+            ],
+            onChanged: (value) {},
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Landmark or Sign of the Area',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Placeholder for tapping to record mic sound
+            },
+            child: CircleAvatar(
+              backgroundColor: Colors.blue,
+              child: Icon(Icons.mic),
+            ),
+            style: ElevatedButton.styleFrom(
+              shape: CircleBorder(),
+              padding: EdgeInsets.all(20),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Placeholder for submitting complaint
+              _handleSubmit();
+            },
+            child: Text('Submit'),
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _handleSubmit() {
+    // Placeholder for handling form submission
+    // Reset the form visibility after submission
+    setState(() {
+      _showForm = false;
+    });
   }
 }
