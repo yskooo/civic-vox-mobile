333abb5e8b64438e7d49adfb1e0738e85bf28fb5
Harold Martin Patacsil <90546802+yskooo@users.noreply.github.com>
2024-04-17 00:42:02 +0800

    c

--- lib/screens/community_view.dart
+++ lib/screens/community_view.dart
@@ -69,52 +69,11 @@ class _CommunityViewState extends State<CommunityView> {
   Widget _buildBottomNavigationBar() {
     return ClipRRect(
       borderRadius: BorderRadius.circular(20), // Clip rounded corners
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-          decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.3), // Glass effect color
-            borderRadius: BorderRadius.circular(20), // Rounded corners
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.2),
-                spreadRadius: 2,
-                blurRadius: 5,
-                offset: Offset(0, 3),
-              ),
-            ], // Add boxShadow for glass effect
-          ),
-          child: BottomNavigationBar(
-            currentIndex: _currentIndex,
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            },
-            items: [
-              _buildNavigationBarItem(Icons.home, 'Home'),
-              _buildNavigationBarItem(Icons.group, 'Complaints'),
-              _buildNavigationBarItem(Icons.notifications, 'Notifications'),
-              _buildNavigationBarItem(Icons.person, 'Profile'),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  BottomNavigationBarItem _buildNavigationBarItem(
-      IconData icon, String label) {
-    return BottomNavigationBarItem(
-      icon: Container(
-        padding: EdgeInsets.all(12),
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
         decoration: BoxDecoration(
-          // color: Colors.transparent,
-          borderRadius: BorderRadius.circular(20),
-          // Rounded corners
-          // border: Border.all(color: Colors.black, width: 1),
-          // Border for selected icon
+          color: Colors.white.withOpacity(0.3), // Glass effect color
+          borderRadius: BorderRadius.circular(20), // Rounded corners
           boxShadow: [
             BoxShadow(
               color: Colors.black.withOpacity(0.2),
@@ -124,11 +83,52 @@ class _CommunityViewState extends State<CommunityView> {
             ),
           ], // Add boxShadow for glass effect
         ),
-        child: Icon(icon),
+        child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            _buildNavigationBarItem(Icons.home, 'Home', true),
+            _buildNavigationBarItem(Icons.checklist_rounded, 'Complaints', true),
+            _buildNavigationBarItem(Icons.notifications, 'Notifications', true),
+            _buildNavigationBarItem(Icons.person, 'Profile', true),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavigationBarItem(
+      IconData icon, String label, bool outlined) {
+    return BottomNavigationBarItem(
+      icon: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: outlined ? null : Colors.white.withOpacity(0.5), // Glass effect color if not outlined
+          borderRadius: BorderRadius.circular(20),
+          border: outlined
+              ? Border.all(color: Colors.black, width: 1) // Add border if outlined
+              : null,
+          boxShadow: outlined
+              ? [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ]
+              : null, // Add boxShadow for glass effect if not outlined
+        ),
+        child: Icon(icon, color: outlined ? Colors.black : null),
       ),
       label: label,
     );
   }
+
 
   Widget _buildGlassTitle(String title) {
     return Container(
